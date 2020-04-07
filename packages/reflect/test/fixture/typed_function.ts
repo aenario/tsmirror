051070a99b3f0346dbfb37db601a5210f600fc85
Romain <aenario@gmail.com>
2020-04-07 16:57:48 +0200

    Add test on Kind.TypeParameter

---
+++ packages/reflect/test/fixture/typed_function.ts
@@ -0,0 +1,20 @@
+import { reflect } from '../../src/index'
+import { Kind } from '../../src/type'
+
+function A<T>(a: T): T {
+    return a
+}
+
+export const result = reflect(A)
+
+export const expected = {
+    kind: Kind.Function,
+    name: 'A',
+    signatures: [{
+        parameters: [{
+            name: "a",
+            type: { kind: Kind.TypeParameter, name: 'T' }
+        }],
+        returnType: { kind: Kind.TypeParameter, name: 'T' }
+    }]
+}
